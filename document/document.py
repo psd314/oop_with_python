48ac9d031f0065d315923f109b1b3064d1adbf0f
Philippe Dixon -X (phidixon - TEKSYSTEMS INC at Cisco) <phidixon@cisco.com>
2019-07-29 08:16:46 -0400

    chapter 5

---
+++ document/document.py
@@ -0,0 +1,24 @@
+from cursor import Cursor
+
+
+class Document:
+    def __init__(self):
+        self.characters = []
+        self.cursor = Cursor(self)
+        self.filename = ''
+
+    @property
+    def string(self):
+        return "".join(self.characters)
+
+    def insert(self, character):
+        self.characters.insert(self.cursor.position, character)
+        self.cursor.forward()
+
+    def delete(self):
+        del self.characters[self.cursor.position]
+
+    def save(self):
+        f = open(self.filename, 'w')
+        f.write(''.join(self.characters))
+        f.close()
---
+++ zip_processor/scalezip.py
@@ -0,0 +1,15 @@
+from zipprocessor import ZipProcessor
+import sys
+from PIL import Image
+
+
+class ScaleZip(ZipProcessor):
+    def process_files(self):
+        for filename in self.temp_directory.iterdir():
+            im = Image.open(str(filename))
+            scaled = im.resize((640, 480))
+            scaled.save(str(filename))
+
+
+if __name__ == '__main__':
+    ScaleZip(*sys.argv[1:4]).process_zip()
